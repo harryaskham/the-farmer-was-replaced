a840775ef2194de2b9a6b4d7512d4f3c98e005dc
Harry Askham <harryaskham@gmail.com>
2025-11-09 12:51:05 +0000

    [rapid commit; squash later]

--- filler_cactus.py
+++ filler_cactus.py
@@ -5,38 +5,11 @@ from filler_utils import *
 
 
 def filler_cactus(state):
-	def handler(state, results):
-		state, d = wh(state)
-		all_done = True
-		
-		#info(state, ("results", results))
-		for result in values(results):
-		
-			#info(state, ("result", result))
-			for x, y in result:
-				one_result = result[(x, y)]
-				((planted, sorted), row) = one_result
-				if x == d - 1:
-					for cell in row:
-						state["grid"][(x, y)] = cell
-				if planted or sorted:
-					all_done = False
-				
-		if not all_done:
-			return state
-			
-		return dos(state, [
-			[try_harvest]
-		])
-			
-	return fill_rows(state, [dos, [
-		[sense, [Sensing.DIRECTIONAL]],
-		[wait_secsM, state["id"] / 32.0],
-		[liftA2, [pair],
-			[liftA2, [pair],
-				[plant_one, E.Cactus],
-				[emplace_cactus, [West]]
-			],
-			[get_row]
-		]
-	]], handler)
+	return fill_rows(
+				state,
+				[dos, [
+						[plant_one, E.Cactus],
+						[get_row]
+				]],
+				[bind, [popret], [merge_row]]
+		)
--- filler_utils.py
+++ filler_utils.py
@@ -20,6 +20,38 @@ def result_list(state, results):
 	for child_id in results:
 		xs.append(results[child_id])
 	return pure(state, xs)
+
+def merge_rows(state, results):
+		for result in values(results):
+				for cell in results:
+						state["grid"][(cell["x"], cell["y"])] = cell
+		return state
+
+def merge_row(state, r):
+        i, result = r
+        for cell in result:
+	        state["grid"][(cell["x"], cell["y"])] = cell
+	return state
+
+def fill_row(state, f, handler=None, n=None):
+	if n == None:
+		n = state["max_drones"]
+	state, d = wh(state)
+	def p(y):
+		return [boxloop, [0, y, d, 1], f, [0, y], False]
+	for y in range(1, min(d, n)):
+		state = spawnM(state, p(y))
+	state, results0 = dos(state, [p(0)])
+	if handler == None:
+		return state
+	state, results = wait_all(state)
+	results[state["id"]] = results0
+        for i in results:
+                dos(state, [
+                        [pushret, (i, results[i]],
+                        [handler]
+                ])
+        return state
 	
 def fill_rows(state, f, handler=None, n=None):
 	if n == None:
@@ -35,3 +67,11 @@ def fill_rows(state, f, handler=None, n=None):
 	state, results = wait_all(state)
 	results[state["id"]] = results0
 	return handler(state, results)
+
+def fill_rowsM(state, f, handler):
+		def h(state, results):
+				return dos(state, [
+						[pushret, results],
+						[handler]
+				])
+		return fill_rows(state, f, h)
